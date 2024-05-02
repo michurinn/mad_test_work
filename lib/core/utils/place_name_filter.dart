0700c79c6d8089818f4f1297401e931534aae70b
michurinnn <michurinnn@gmail.com>
2024-05-02 13:03:27 +0300

    feat: filter method had been changed

--- lib/core/utils/place_name_filter.dart
+++ lib/core/utils/place_name_filter.dart
@@ -5,11 +5,15 @@ class PlaceNameFilter {
 
   List<Place> filteredPlaces(
       final List<Place> places, final String filterString) {
-    return places.where((place) => place.title.contains(filterString)).toList()
+    return places
+        .where((place) =>
+            place.title.toUpperCase().contains(filterString.toUpperCase()))
+        .toList()
       ..sort(
-        (a, b) => a.title.indexOf(filterString).compareTo(
-              b.title.indexOf(filterString),
-            ),
+        (a, b) =>
+            a.title.toUpperCase().indexOf(filterString.toUpperCase()).compareTo(
+                  b.title.toUpperCase().indexOf(filterString.toUpperCase()),
+                ),
       );
   }
 }
