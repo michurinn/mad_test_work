f236eb5440d2b55f7a98c010dfc9469e39cfa700
michurinnn <michurinnn@gmail.com>
2024-05-02 18:29:06 +0300

    feat: colors for the app have been added

---
+++ lib/src/colors/app_colors.dart
@@ -0,0 +1,8 @@
+import 'package:flutter/material.dart';
+
+class AppColors {
+  static const listTileBackgroundColor = Colors.white;
+  static const customTextSecond = Color(0xff6B6C6C);
+  static const menuActive = Color(0xff4A6FF4);
+  static const background = Color(0xffEEF2FF);
+}
