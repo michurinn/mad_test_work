9f0102abde5ad6d5d520446fff8a3dbca83f8d78
michurinnn <michurinnn@gmail.com>
2024-05-02 13:04:26 +0300

    feat: app textStyles had been added

---
+++ lib/src/typography/app_typography.dart
@@ -0,0 +1,27 @@
+import 'dart:ui';
+
+class AppTypography {
+  final m3TitleMedium = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 16,
+    fontWeight: FontWeight.w500,
+    height: 1.5,
+    letterSpacing: 0.15,
+  );
+
+  final m3labelSmall = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 11,
+    fontWeight: FontWeight.w500,
+    height: 1.45,
+    letterSpacing: 0.5,
+  );
+
+  final m3BodyMedium = TextStyle(
+    fontFamily: 'Roboto',
+    fontSize: 14,
+    fontWeight: FontWeight.w400,
+    height: 1.43,
+    letterSpacing: 0.25,
+  );
+}
