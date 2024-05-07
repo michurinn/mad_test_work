29b0f0371cdfd062adb2fd077b4d30e643e6bb86
michurinnn <michurinnn@gmail.com>
2024-05-07 17:10:08 +0300

    Revert "feat: builsness logic has been added"
    Отмена ошибочного коммита

--- lib/core/architecture/domain/entity/request_operation.dart
+++
@@ -1,7 +0,0 @@
-
-import 'package:mad_test_work/core/architecture/domain/entity/result.dart';
-import 'package:mad_test_work/core/failtures/failures.dart';
-
-/// Typedef for all methods that may fail.
-/// These are mostly repository methods.
-typedef RequestOperation<T> = Future<Result<T, Failure>>;
--- lib/src/find_tickets/domain/repositories/i_requested_repository.dart
+++
@@ -1,6 +0,0 @@
-import 'package:mad_test_work/core/architecture/domain/entity/request_operation.dart';
-import 'package:mad_test_work/src/find_tickets/domain/entities/requested_offer_entity.dart';
-
-abstract interface class IRequestedRepository {
-  RequestOperation<List<RequestedOfferEntity>> getRequestedTickets();
-}
